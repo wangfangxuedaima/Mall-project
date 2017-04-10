72f273e9c328fe3339358f4bf22a7333d9ed0b25
unknown <wangfang31@360buyad.local>
2017-04-10 13:41:32 +0800

    commit

--- app/js/common/Router.js
+++ app/js/common/Router.js
@@ -1,7 +1,7 @@
 /**
  * Created by lyting on 16-4-25.
  */
-define(['backbone'], function (Backbone) {
+define(['backbone'], function(Backbone) {
 
     var Router = Backbone.Router.extend({
 
@@ -9,50 +9,40 @@ define(['backbone'], function (Backbone) {
             '': 'login',
             'login': 'login',
             'main': 'main',
-            'billing':'billing',
+            'billing': 'billing',
             // 'logout': 'logout',
             '*actions': 'defaultAction'
         },
 
         //路由初始化可以做一些事
-        initialize: function () {
-        },
+        initialize: function() {},
 
-        login: function () {
+        login: function() {
             var _url = './moduals/login/view.js';
-            require([_url], function (View) {
+            require([_url], function(View) {
                 var view = new View();
                 view.render();
             });
 
         },
-        main: function () {
+        main: function() {
             var _url = '../../moduals/home/view';
-            require([_url], function (View) {
+            require([_url], function(View) {
                 var view = new View();
                 view.render();
             });
         },
-        billing: function () {
+        billing: function() {
             var _url = '../../moduals/billing/view';
-            require([_url], function (View) {
+            require([_url], function(View) {
                 var view = new View();
                 view.render();
             });
         },
-        // logout: function () {
-        //     storage.remove(system_config.LOGIN_USER_KEY);
-        //     if (storage.isEmpty(system_config.LOGIN_USER_KEY)) {
-        //         router.navigate("", {trigger: true});
-        //     } else {
-        //         router.navigate("main", {trigger: true});
-        //     }
-        //
-        // },
-        defaultAction: function () {
-            router.navigate("main", {trigger: true});
+        defaultAction: function() {
+            router.navigate("main", { trigger: true });
         }
     });
     var router = new Router();
-    return router;    //这里必须的，让路由表执行
+    return router; //这里必须的，让路由表执行
 });
