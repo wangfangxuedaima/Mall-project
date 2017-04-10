20ee76216b164a125ed6163eb4b2fe6c2f3c4d95
unknown <wangfang31@360buyad.local>
2017-04-10 14:00:01 +0800

    remove

--- app/js/common/BaseView.js
+++ app/js/common/BaseView.js
@@ -6,13 +6,13 @@ define([
     'underscore',
     'backbone',
     'framework7'
-], function ($, _, Backbone,Framework7) {
+], function($, _, Backbone, Framework7) {
 
     var BaseView = Backbone.View.extend({
 
         attrs: null,
 
-        initialize: function (attrs) {
+        initialize: function(attrs) {
             console.log(">>> " + this.id);
             this.undelegateEvents();
             this.$el.empty().off();
@@ -20,17 +20,13 @@ define([
                 this.attrs = attrs;
             }
             if (this.model) {
-                this.collection = new Backbone.Collection.extend({model: this.model});
+                this.collection = new Backbone.Collection.extend({ model: this.model });
             }
             if (this.template) {
                 this.$el.html(this.template);
                 this.template = _.template(this.template);
             }
 
-            this.initRouter();
-
-            this.pageInit();
-            this.stopListening();
             if (this.model) {
                 // 注册 model change事件
                 this.listenTo(this.model, "change", this.model_change);
@@ -45,35 +41,8 @@ define([
             this.delegateEvents();
         },
 
-        initRouter: function () {
 
-        },
-
-        pageInit: function () {
-        },
-
-        model_change: function () {
-
-        },
-        collection_add: function () {
-
-        },
-        collection_remove: function () {
-
-        },
-        collection_reset: function () {
-
-        },
-
-        initOtherView: function () {
-
-        },
-
-        initPlugins: function () {
-
-        },
-
-        render: function () {
+        render: function() {
             var _data = this.collection || this.model;
             var dataset = _data ? _data.toJSON() : {};
             this.$el.html(this.template(dataset));
--- app/js/config.js
+++ app/js/config.js
@@ -15,9 +15,9 @@ require.config({
         "validation": "backbone-validation",
         "md5": "jQuery.md5",
         "common": "common",
-        'framework7':'../framework7/js/framework7.min',
+        'framework7': '../framework7/js/framework7.min',
         "loading": "../loading/jquery.showLoading.min",
-        'keypad':'../framework7-keypad/framework7.keypad.min',
+        'keypad': '../framework7-keypad/framework7.keypad.min',
         //"light7": "../light7/js/light7.min",
         //"citypicker": "../light7/js/light7-city-picker.min",
         //"swipeout": "../light7/js/light7-swipeout",
@@ -54,11 +54,11 @@ require.config({
         "md5": {
             "deps": ["jquery"],
         },
-        "framework7":{
-            "deps":["css!../framework7/css/framework7.material.css","css!../framework7/css/framework7.material.colors.css"]
+        "framework7": {
+            "deps": ["css!../framework7/css/framework7.material.css", "css!../framework7/css/framework7.material.colors.css"]
         },
-        "keypad":{
-            "deps":["framework7","css!../framework7-keypad/framework7.keypad.min.css"]
+        "keypad": {
+            "deps": ["framework7", "css!../framework7-keypad/framework7.keypad.min.css"]
         },
         "loading": {
             "deps": [
@@ -104,9 +104,9 @@ require([
     '../../moduals/setting/view',
     '../../moduals/itemlist/view'
 
-], function ($,_, Backbone, common, framework7, BaseRouter, validation, serializeObject, storage,loading,HomeView,OnlineOrderView,OnlineServiceView,RechargeView,ReturnView,SettingView,ItemListView) {
+], function($, _, Backbone, common, framework7, BaseRouter, validation, serializeObject, storage, loading, HomeView, OnlineOrderView, OnlineServiceView, RechargeView, ReturnView, SettingView, ItemListView) {
 
-    Backbone.history.start();   //开始监控url变化
+    Backbone.history.start(); //开始监控url变化
     _.extend(Backbone.Model.prototype, Backbone.Validation.mixin);
 
     //  validation 表单验证 页面布局显示
@@ -143,11 +143,11 @@ require([
 
     window.f7app = new Framework7({
         swipePanel: false,
-        material:true,
-        materialRipple:true,
-        materialRippleElements:'.ripple',
-        fastClicks:true,
-        router:false,
+        material: true,
+        materialRipple: true,
+        materialRippleElements: '.ripple',
+        fastClicks: true,
+        router: false,
     });
 
     window.$$ = Dom7;
@@ -157,8 +157,8 @@ require([
         window.storage.set(system_config.IS_FIRST_KEY, true);
     }
     // 重写控制台输出方法，添加调试标志
-    console.log = (function (oriLogFunc) {
-        return function (str) {
+    console.log = (function(oriLogFunc) {
+        return function(str) {
             if (debug) {
                 oriLogFunc.call(console, str);
             }
@@ -166,46 +166,46 @@ require([
     })(console.log);
 
     window.TOKEN = {
-        set: function (token) {
+        set: function(token) {
             window.storage.set(system_config.TOKEN_KEY, token);
         },
-        get: function () {
+        get: function() {
             return window.storage.get(system_config.TOKEN_KEY);
         },
-        remove: function () {
+        remove: function() {
             window.storage.remove(system_config.TOKEN_KEY);
         }
     };
 
     window.LOGIN_USER = {
-        set: function (login_user) {
+        set: function(login_user) {
             window.storage.set(window.system_config.LOGIN_USER_KEY, login_user);
         },
-        get: function () {
+        get: function() {
             window.storage.get(window.system_config.LOGIN_USER_KEY);
         }
     };
 
     window.POS_KEY = {
-        set: function (key) {
+        set: function(key) {
             // todo 预置方法
         },
-        get: function () {
+        get: function() {
             var pos_key = window.storage.get(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.POS_KEY);
             return pos_key;
         }
     };
 
     window.GATEWAY = {
-        set: function () {
+        set: function() {
 
         },
-        get: function () {
+        get: function() {
             return window.storage.get(system_config.SETTING_DATA_KEY, system_config.INIT_DATA_KEY, system_config.GATEWAY_KEY);
         }
     };
 
-    window.common_params = function (params) {
+    window.common_params = function(params) {
         var _p = $.isEmptyObject(params) ? {} : params;
         return $.extend(_p, {
             "poskey": window.POS_KEY.get(),
@@ -216,131 +216,78 @@ require([
     };
 
 
-    // window.datetimePlugins = function (obj, isUseTime) {
-    //
-    //     var curr = new Date().getFullYear();
-    //
-    //     var opt = {};
-    //     if (isUseTime) {
-    //         opt.preset = {preset: 'datetime'};
-    //     } else {
-    //         opt.preset = {preset: 'date'};
-    //     }
-    //
-    //     opt.time = {preset: 'time'};
-    //
-    //     opt.default = {
-    //         theme: 'ios light', //皮肤样式
-    //         display: 'modal', //显示方式
-    //         mode: 'scroller', //日期选择模式
-    //         dateFormat: 'yyyy-mm-dd',
-    //         lang: 'zh',
-    //         showNow: true,
-    //         nowText: "今天",
-    //         stepMinute: 5,
-    //         startYear: curr - 10, //开始年份
-    //         endYear: curr + 10 //结束年份
-    //     };
-    //
-    //     obj.scroller('destroy').scroller($.extend(opt['preset'], opt['default']));
-    // };
+
 
     // 定义系统router
     window.router = BaseRouter;
 
-    $(document).ready(function () {
-         window.loading = {
-             show: function (obj) {
-                 var target = obj || $("body");
-                 target.showLoading({
-                     overlayWidth: $(document).width(),
-                     overlayHeight: $(document).height()
-                 });
-             },
+    $(document).ready(function() {
+        window.loading = {
+            show: function(obj) {
+                var target = obj || $("body");
+                target.showLoading({
+                    overlayWidth: $(document).width(),
+                    overlayHeight: $(document).height()
+                });
+            },
 
-             hide: function (obj) {
-                 var target = obj || $("body");
-                 target.hideLoading();
-             }
-         };
+            hide: function(obj) {
+                var target = obj || $("body");
+                target.hideLoading();
+            }
+        };
 
-        /**
-         * 设置屏幕宽高
-         */
-        // var setScreenWH = function () {
-        //     var dw = $(document).width(),
-        //         dh = $(document).height();
-        //     var cw = $("#container").width(),
-        //         ch = $("#container").height();
-        //     var cwd = dw - cw;
-        //     var chd = dh - ch;
-        //     var cl = 0;
-        //     var ct = 0;
-        //     if (cwd > 0) {
-        //         cl = cwd / 2;
-        //     }
-        //     if (chd > 0) {
-        //         ct = chd / 2;
-        //     }
-        //     $("#container").css({
-        //         "left": cl,
-        //         "top": ct
-        //     });
-        // };
-
-        // setScreenWH();
 
     });
 
     var itemListView = new ItemListView();
     itemListView.render();
 
-    $('.toHome').bind('click', function () {
+    $('.toHome').bind('click', function() {
         f7app.closePanel();
         var homeView = new HomeView();
         homeView.render();
 
     });
-    $('.toOnlineOrder').bind('click', function () {
+    $('.toOnlineOrder').bind('click', function() {
         f7app.closePanel();
         var onlineOrderView = new OnlineOrderView();
         onlineOrderView.render();
 
     });
 
-    $('.toOnlineService').bind('click', function () {
+    $('.toOnlineService').bind('click', function() {
         f7app.closePanel();
         var onlineServiceView = new OnlineServiceView();
         onlineServiceView.render();
 
     });
-    $('.toRecharge').bind('click', function () {
+    $('.toRecharge').bind('click', function() {
         f7app.closePanel();
         var rechargeView = new RechargeView();
         rechargeView.render();
 
     });
-    $('.toReturn').bind('click', function () {
+    $('.toReturn').bind('click', function() {
         f7app.closePanel();
         var returnView = new ReturnView();
         returnView.render();
 
     });
-    $('.toSetting').bind('click', function () {
+    $('.toSetting').bind('click', function() {
         f7app.closePanel();
         var settingView = new SettingView();
         settingView.render();
 
     });
-    $('.toLogout').bind('click', function () {
+    $('.toLogout').bind('click', function() {
         console.log('logout');
         f7app.closePanel();
-        router.navigate("", {trigger: true});
+        router.navigate("", { trigger: true });
     });
 
-    $('.panelClose').bind('click', function () {
+    $('.panelClose').bind('click', function() {
         f7app.closePanel();
     });
 
 });
-
